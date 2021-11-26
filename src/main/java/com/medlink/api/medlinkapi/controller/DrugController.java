1f1bda4c3e50de4a6013232d4173a3920858895a
jimmianh <jimmianh@1807@gmail.com>
2021-11-26 20:52:25 +0700

    tim hieu crud

--- src/main/java/com/medlink/api/medlinkapi/controller/DrugController.java
+++ src/main/java/com/medlink/api/medlinkapi/controller/DrugController.java
@@ -1,17 +1,19 @@
 package com.medlink.api.medlinkapi.controller;
 
-import com.medlink.api.medlinkapi.dao.DrugDAO;
 import com.medlink.api.medlinkapi.model.Drug;
+import com.medlink.api.medlinkapi.repository.DrugEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.*;
 
 @RestController
 public class DrugController {
     @Autowired
-    private DrugDAO drugDAO;
+    private DrugEntityRepository drugEntityRepository;
 
     //Show list drug
     @RequestMapping(value = "/drug", 
@@ -20,18 +22,16 @@ public class DrugController {
                     MediaType.APPLICATION_XML_VALUE })
     @ResponseBody
     public List<Drug> getDrugs() {
-        List<Drug> list = drugDAO.getAllDrugs();
+        List<Drug> list = drugEntityRepository.showAll();
         return list;
     }
 
     //FindByID
-    @RequestMapping(value = "/drug/{drugID}", 
-            method = RequestMethod.GET, 
-            produces = { MediaType.APPLICATION_JSON_VALUE, 
-                    MediaType.APPLICATION_XML_VALUE })
+    @GetMapping(value = "/drug/{drug_id}")
     @ResponseBody
-    public Drug getDrug(@PathVariable("drugID") String drugID) {
-        return drugDAO.getDrug(drugID);
+    public Optional<Drug>  getDrug(@PathVariable int drug_id) {
+        Optional<Drug> drug = drugEntityRepository.findById(drug_id);
+        return drug;
     }
 
     //Create new drug
@@ -40,35 +40,35 @@ public class DrugController {
             produces = { MediaType.APPLICATION_JSON_VALUE, 
                     MediaType.APPLICATION_XML_VALUE })
     @ResponseBody
-    public Drug createDrug(@RequestBody Drug drug) {
+    public int createDrug(@RequestBody Drug drug) {
 
-        System.out.println("(Service Side) Creating Drug: " + drug.getDrugID());
+        System.out.println("(Service Side) Creating Drug: " + drug.getDrug_id() + "," + drug.getDrg_drug_name() + "," + drug.getUnit_name() + "," + drug.getPrice() + "," + drug.getQuantity());
 
-        return drugDAO.addDrug(drug);
+        return drugEntityRepository.insert(drug);
     }
 
-    //update by id
-    @RequestMapping(value = "/drug/update/{drugID}",
-            method = RequestMethod.PUT, 
-            produces = { MediaType.APPLICATION_JSON_VALUE, 
-                    MediaType.APPLICATION_XML_VALUE })
-    @ResponseBody
-    public Drug updateDrug(@RequestBody Drug drug) {
-
-        System.out.println("(Service Side) Editing Drug: " + drug.getDrugID());
-
-        return drugDAO.updateDrug(drug);
-    }
+//    //update by id
+//    @RequestMapping(value = "/drug/update/{drug_id}",
+//            method = RequestMethod.PUT,
+//            produces = { MediaType.APPLICATION_JSON_VALUE,
+//                    MediaType.APPLICATION_XML_VALUE })
+//    @ResponseBody
+//    public int updateDrug(@PathVariable("drug_id")  String drg_drug_name, String unit_name, BigDecimal price, BigDecimal quantity, int drug_id) {
+//
+//        System.out.println("(Service Side) Editing Drug: " + drug_id);
+//
+//        return drugEntityRepository.updateById(drg_drug_name, unit_name,price,quantity,drug_id);
+//    }
 
     //Delete by id
-    @RequestMapping(value = "/drug/{drugID}", 
+    @RequestMapping(value = "/drug/{drug_id}", 
             method = RequestMethod.DELETE, 
             produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     @ResponseBody
-    public void deleteDrug(@PathVariable("drugID") String drugID) {
+    public void deleteDrug(@PathVariable("drug_id") String drug_id) {
 
-        System.out.println("(Service Side) Deleting Drug: " + drugID);
+        System.out.println("(Service Side) Deleting Drug: " + drug_id);
 
-        drugDAO.deleteDrug(drugID);
+        drugEntityRepository.deleteById(drug_id);
     }
 }
--- src/main/java/com/medlink/api/medlinkapi/model/Drug.java
+++ src/main/java/com/medlink/api/medlinkapi/model/Drug.java
@@ -1,61 +1,73 @@
 package com.medlink.api.medlinkapi.model;
 
+import java.math.BigDecimal;
+
 public class Drug {
-    private String drugID;
-    private String name;
-    private String unit;
-    private double price;
-    private int quantity;
+    private int drug_id;
+    private int drg_store_id;
+    private String drg_drug_name;
+    private String unit_name;
+    private BigDecimal price;
+    private BigDecimal quantity;
+
+    public Drug(int drug_id, int drg_store_id, String drg_drug_name, String unit_name, BigDecimal price, BigDecimal quantity) {
+        this.drug_id = drug_id;
+        this.drg_store_id = drg_store_id;
+        this.drg_drug_name = drg_drug_name;
+        this.unit_name = unit_name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 
     public Drug() {
     }
 
-    public Drug(String drugID, String name, String unit, double price, int quantity) {
-        this.drugID = drugID;
-        this.name = name;
-        this.unit = unit;
-        this.price = price;
-        this.quantity = quantity;
-    }
-    public String getName() {
-        return name;
+    public int getDrug_id() {
+        return drug_id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setDrug_id(int drug_id) {
+        this.drug_id = drug_id;
     }
 
-
-    public int getQuantity() {
-        return quantity;
+    public int getDrg_store_id() {
+        return drg_store_id;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    public void setDrg_store_id(int drg_store_id) {
+        this.drg_store_id = drg_store_id;
     }
 
-    public String getDrugID() {
-        return drugID;
+    public String getDrg_drug_name() {
+        return drg_drug_name;
     }
 
-    public void setDrugID(String drugID) {
-        this.drugID = drugID;
+    public void setDrg_drug_name(String drg_drug_name) {
+        this.drg_drug_name = drg_drug_name;
     }
 
-    public String getUnit() {
-        return unit;
+    public String getUnit_name() {
+        return unit_name;
     }
 
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-    public void setPrice(double price) {
-        this.price = price;
+    public void setUnit_name(String unit_name) {
+        this.unit_name = unit_name;
     }
 
-    public double getPrice() {
+    public BigDecimal getPrice() {
         return price;
     }
 
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
 }
 
